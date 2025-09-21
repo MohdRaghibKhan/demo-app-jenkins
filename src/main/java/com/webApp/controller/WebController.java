d792511f2cada17424788b2c7cb7cc66a5d3d1b8
Raghib <mohammad.raghib@scoreme.in>
2025-09-21 21:02:56 +0530

    second commit

--- src/main/java/com/webApp/controller/WebController.java
+++ src/main/java/com/webApp/controller/WebController.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Mohd Raghib Khan
  */
 @RestController
+@RequestMapping("/api")
 public class WebController {
-	
-@GetMapping
+
+	@GetMapping("/hello")
 	public List<String> getData() {
-		
-		List<String> list=new ArrayList<>();
+
+		List<String> list = new ArrayList<>();
 		list.add("hello this is from jenkins");
 		list.add("this is our first jenkins project ");
 		return list;
