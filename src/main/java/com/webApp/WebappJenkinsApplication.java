7e9f903f117c273a0fd5d01a04508c7ac9fa6bee
Raghib <mohammad.raghib@scoreme.in>
2025-09-21 21:14:06 +0530

    Initial commit

--- src/main/java/com/webApp/WebappJenkinsApplication.java
+++ src/main/java/com/webApp/WebappJenkinsApplication.java
@@ -2,18 +2,11 @@ package com.webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebappJenkinsApplication extends SpringBootServletInitializer {
+public class WebappJenkinsApplication {
 	public static void main(String[] args) {
         SpringApplication.run(WebappJenkinsApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(WebappJenkinsApplication.class);
     }
 
 }
