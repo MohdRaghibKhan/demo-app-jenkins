ddd09f7b7deea1bf3f2ead88d2096fa3f8840371
Raghib <mohammad.raghib@scoreme.in>
2025-09-28 15:44:18 +0530

    new

--- src/main/java/com/webApp/controller/WebController.java
+++ src/main/java/com/webApp/controller/WebController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api")
-public class WebController {
+public class WebController { 
 
 	@GetMapping("/hello")
 	public List<String> getData() {
